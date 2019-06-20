5ea354ae5c42bd44ad1e2e492211b2348f6ec056
OMARIHAMZA <36658089+OMARIHAMZA@users.noreply.github.com>
2019-06-20 11:40:41 +0300

    post-interview commit

--- Main.rb
+++ Main.rb
@@ -22,32 +22,83 @@ begin
   selection_columns = []
   records = []
 
-  join_type = "null"
-  employees_table_location, employees_field_terminator = ExecutionPlanUtilities.get_table_location("employees")
-  employees_csv_files = ExecutionPlanUtilities.get_csv_files(employees_table_location, "employees")
-  employees_file_index = 0
-  employees_pos = 0
 
-  until employees_file_index == employees_csv_files.length
+  ExecutionPlanUtilities::write_to_execution_plan("Nested Loop Join")
+  join_type = "LEFTJOIN"
+  employees_1_department_id_index=  ExecutionPlanUtilities::get_column_index("employees", "department_id")
+  departments_0_department_id_index=  ExecutionPlanUtilities::get_column_index("departments", "department_id")
 
-    employees_line, employees_file_index, employees_pos = ExecutionPlanUtilities.read_record(employees_table_location, employees_csv_files, employees_file_index, employees_pos,  employees_field_terminator)
 
-    records << employees_line.chomp if true
+
+  ExecutionPlanUtilities::write_to_execution_plan("Nested Loop Join")
+
+
+  employees_1_table_location, employees_field_terminator = ExecutionPlanUtilities.get_table_location("employees")
+  employees_1_csv_files = ExecutionPlanUtilities.get_csv_files(employees_1_table_location, "employees")
+  employees_1_file_index = 0
+  employees_1_pos = 0
+
+  until employees_1_file_index == employees_1_csv_files.length
+
+    employees_1_line, employees_1_file_index, employees_1_pos =
+        ExecutionPlanUtilities.read_record(employees_1_table_location, employees_1_csv_files, employees_1_file_index, employees_1_pos, employees_field_terminator)
+
+    record_1 = "" + employees_1_line.chomp
+
+    employees_1_joined_flag = false
+
+    departments_0_table_location, departments_field_terminator = ExecutionPlanUtilities.get_table_location("departments")
+    departments_0_csv_files = ExecutionPlanUtilities.get_csv_files(departments_0_table_location, "departments")
+    departments_0_file_index = 0
+    departments_0_pos = 0
+
+    until departments_0_file_index == departments_0_csv_files.length
+
+      departments_0_line, departments_0_file_index, departments_0_pos = ExecutionPlanUtilities.read_record(departments_0_table_location, departments_0_csv_files, departments_0_file_index, departments_0_pos, departments_field_terminator)
+      record_0 = record_1 + "," + departments_0_line.chomp
+
+      departments_0_joined_flag = false
+
+      if employees_1_line.split(",")[employees_1_department_id_index].strip.to_i==departments_0_line.split(",")[departments_0_department_id_index].strip.to_i
+
+        employees_1_joined_flag = true
+
+        records << record_0
+
+      end
+
+
+    end
+
+    records << employees_1_line.chomp + ("," * 3) unless employees_1_joined_flag
+
 
   end
 
-  having_conditions = []
-  aggregation_columns = []
+  FileUtils.mkdir_p("prev/")
 
+  prev_outer_file = File.open("prev/prev_file.csv", "w")
+
+  prev_outer_file.puts records
+
+  prev_outer_file.close
+
+  prev_outer_length = 3 + 4
+
+
+  having_conditions = []
+  aggregation_columns = [{:function=>:SUM,:index=>ExecutionPlanUtilities::get_column_index("employees", "salary"),:type=>:INT,:distinct=>nil},]
+
+  grouping_columns << ExecutionPlanUtilities::get_column_index("employees", "department_id") + 0
 
 
   if aggregation_columns.empty?
 
+    selection_columns << 0 + ExecutionPlanUtilities::get_column_index("employees", "department_id")
 
   end
 
   records.sort_by!{|record| [ ]}
-
   unless selection_columns.empty?
     records.map!{|record| record.split(",").values_at(*selection_columns).join(",")}
   end
@@ -67,7 +118,9 @@ begin
 
   end
 
-  puts records if aggregation_columns.empty? && ! false
+  puts File.read(ExecutionPlanUtilities::EXECUTION_PLAN_FILE_NAME) if true
+
+  puts records if aggregation_columns.empty? && ! false && ! true
 
   ExecutionPlanUtilities::write_query_to_file(records, query_counter)
   query_counter += 1
